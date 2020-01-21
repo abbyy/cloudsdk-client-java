fd2a27ec67bc017ee11cfd15864a1b2d8a2e706e
Anna Savinova <anna.savinova@abbyy.com>
2020-01-22 02:23:00 +0300

    Rename package
    Signed-off-by: Anna Savinova <anna.savinova@abbyy.com>

--- src/test/java/abbyy/cloudsdk/v2/client/ErrorTarget.java
+++
@@ -1,9 +0,0 @@
-package abbyy.cloudsdk.v2.client;
-
-public class ErrorTarget {
-    public static final String File = "File";
-    public static final String TaskId = "TaskId";
-    public static final String Language = "Language";
-    public static final String Description = "Description";
-    public static final String Region = "Region";
-}
---
+++ src/test/java/com/cloudsdk/abbyy/v2/client/ErrorTarget.java
@@ -0,0 +1,9 @@
+package com.cloudsdk.abbyy.v2.client;
+
+public class ErrorTarget {
+    public static final String File = "File";
+    public static final String TaskId = "TaskId";
+    public static final String Language = "Language";
+    public static final String Description = "Description";
+    public static final String Region = "Region";
+}
